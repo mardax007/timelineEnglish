ee941384356dffcee35fbc3e8b121306988d9167
Mardax <62964422+mardax007@users.noreply.github.com>
2022-12-22 11:05:38 +0100

    feat: :sparkles: fitst release

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,110 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "./styling.css";
+	import {
+		Timeline,
+		TimelineItem,
+		TimelineSeparator,
+		TimelineDot,
+		TimelineConnector,
+		TimelineContent,
+		TimelineOppositeContent
+	} from 'svelte-vertical-timeline';
+
+	const options = [
+    {
+        "title":"Norman Conquest of England",
+        "time":"1066"
+    },
+    {
+        "title":"First Crusade",
+        "time":"1096"
+    },
+    {
+        "title":"Henry I becomes King of England",
+        "time":"1100"
+    },
+    {
+        "title":"Stephen becomes King of England",
+        "time":"1135"
+    },
+    {
+        "title":"Henry II becomes King of England",
+        "time":"1154"
+    },
+    {
+        "title":"Thomas Becket assassinated",
+        "time":"1170"
+    },
+    {
+        "title":"Richard the Lionheart becomes King of England",
+        "time":"1189"
+    },
+    {
+        "title":"Signing of the Magna Carta",
+        "time":"1215"
+    },
+    {
+        "title":"King John becomes King of England",
+        "time":"1216"
+    },
+    {
+        "title":"Battle of Lincoln",
+        "time":"1217"
+    },
+    {
+        "title":"Edward I becomes King of England",
+        "time":"1272"
+    },
+    {
+        "title":"Edward I expels Jews from England",
+        "time":"1290"
+    },
+    {
+        "title":"Beginning of the Hundred Years' War between England and France",
+        "time":"1337"
+    },
+    {
+        "title":"Outbreak of the Black Death in Europe",
+        "time":"1348"
+    },
+    {
+        "title":"Peasants' Revolt in England",
+        "time":"1381"
+    },
+    {
+        "title":"Henry IV becomes King of England",
+        "time":"1399"
+    }
+];
+</script>
+
+<Timeline position="alternate" style={"border: solid 1px #dbdbdb; padding: 50px 0; border-radius: 2%;"}>
+	{#each options as option}
+		<TimelineItem>
+			<TimelineOppositeContent slot="opposite-content">
+				<p>{option.time}</p>
+			</TimelineOppositeContent>
+			<TimelineSeparator>
+				<TimelineDot style={'background-color: #7CD5E2;'}/>
+				<TimelineConnector />
+			</TimelineSeparator>
+			<TimelineContent>
+				<h3>{option.title}</h3>
+			</TimelineContent>
+		</TimelineItem>
+	{/each}
+</Timeline>
+
+<style>
+	h3 {
+		letter-spacing: 1.5px;
+		margin: 0;
+		padding: 0;
+	}
+	
+	p {
+		letter-spacing: 1.5px;
+		margin: 0;
+		color: grey;
+	}
+</style>
---
+++ src/routes/chaucer/+page.svelte
@@ -0,0 +1,94 @@
+<script>
+    import "../styling.css";
+	import {
+		Timeline,
+		TimelineItem,
+		TimelineSeparator,
+		TimelineDot,
+		TimelineConnector,
+		TimelineContent,
+		TimelineOppositeContent
+	} from 'svelte-vertical-timeline';
+
+	const options = [
+    {
+        "title":"Chaucer is born in London, England",
+        "time":"1343"
+    },
+    {
+        "title":"Chaucer serves as a page to Elizabeth, Countess of Ulster",
+        "time":"1357"
+    },
+    {
+        "title":"Chaucer joins the English army and fights in the Hundred Years' War",
+        "time":"1359"
+    },
+    {
+        "title":"Chaucer marries Philippa de Roet, a lady-in-waiting to Queen Philippa",
+        "time":"1366"
+    },
+    {
+        "title":"Chaucer becomes a member of Parliament",
+        "time":"1374"
+    },
+    {
+        "title":"Chaucer becomes a clerk of the king's works",
+        "time":"1378"
+    },
+    {
+        "title":"Chaucer becomes a justice of the peace",
+        "time":"1380"
+    },
+    {
+        "title":"Chaucer becomes a comptroller of the customs in the port of London",
+        "time":"1386"
+    },
+    {
+        "title":"Chaucer becomes deputy forester of North Petherton Park in Somerset",
+        "time":"1389"
+    },
+    {
+        "title":"Chaucer becomes a member of the royal household",
+        "time":"1391"
+    },
+    {
+        "title":"Chaucer begins writing The Canterbury Tales",
+        "time":"1400"
+    },
+    {
+        "title":"Chaucer dies in London, England",
+        "time":"1400"
+    }
+];
+</script>
+
+<Timeline position="alternate" style={"border: solid 1px #dbdbdb; padding: 50px 0; border-radius: 2%;"}>
+	{#each options as option}
+		<TimelineItem>
+			<TimelineOppositeContent slot="opposite-content">
+				<p>{option.time}</p>
+			</TimelineOppositeContent>
+			<TimelineSeparator>
+				<TimelineDot style={'background-color: #7CD5E2;'}/>
+				<TimelineConnector />
+			</TimelineSeparator>
+			<TimelineContent>
+				<h3>{option.title}</h3>
+			</TimelineContent>
+		</TimelineItem>
+	{/each}
+</Timeline>
+
+<style>
+	h3 {
+		letter-spacing: 1.5px;
+		margin: 0;
+		padding: 0;
+	}
+	
+	p {
+		letter-spacing: 1.5px;
+		margin: 0;
+		color: grey;
+	}
+</style>
